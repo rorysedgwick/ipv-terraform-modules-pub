815e434e32464f84d3ded2d6e07a5b6140452d57
Identity Pod Module Release <rory.sedgwick@digital.cabinet-office.gov.uk>
2024-06-12 15:24:42 +0000

    Publishing: vpc/v2.5.11

--- vpc/variables.tf
+++ vpc/variables.tf
@@ -10,13 +10,13 @@ variable "template_url" {
 variable "template_data" {
   type = object({
     bucket  = optional(string, "template-storage-templatebucket-1upzyw6v9cs42")
-    version = optional(string, "v2.5.10")
+    version = optional(string, "v2.5.11")
     object  = optional(string, "vpc/template.yaml")
   })
 
   default = {
     bucket  = "template-storage-templatebucket-1upzyw6v9cs42"
-    version = "v2.5.10"
+    version = "v2.5.11"
     object  = "vpc/template.yaml"
   }
 }
